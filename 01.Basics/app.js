060b31112b54d627cdf50e4334ae890319f5b642
kenziikun <agissmubarok@gmail.com>
2024-12-27 15:16:52 +0700

    learn/01.basics-huxnwebdev/basics

---
+++ 01.Basics/app.js
@@ -0,0 +1,38 @@
+// log -> console.log
+// clg -> console.log(value)
+
+// variable
+// (reserve keyword) (variable name) (type of value)
+
+// declare variable
+let name;
+
+// assign value
+let mango;
+mango = "mango";
+
+let agisKenzi = "mubarok";
+agisKenzi = "superman";
+
+// jangan declare variable dengan angka di depan dan simbol, karena akan memunculkan error
+// ketika reserve keyword itu berbentuk const, maka kita tidak bisa mengubah value nya, tapi jika itu let maka bisa dirubah
+const sideBar = "ini sidebar";
+
+// primitive types
+// 1. Number
+
+let num = 10;
+// console.log(typeof num); //ini adalah cara untuk mengetahui type dari sebuah variable
+
+// basic math
+// console.log(10 + 10);
+// console.log(10 - 10);
+// console.log(10 * 10);
+// console.log(10 / 10);
+// console.log(10 % 10);
+// console.log(3 ** 3);
+
+let counter = 10;
+// counter++; //ini adalah cara increment 1 nilai
+counter--; //ini adalah cara decrement mengurangi 1 nilai
+console.log(counter);
