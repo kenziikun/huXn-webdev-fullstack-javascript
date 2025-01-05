3dc42ac86cf83564cb64ba7c5133a83c7a005622
kenziikun <agissmubarok@gmail.com>
2025-01-05 08:13:08 +0700

    /learn/huxn webdev/jam ke 3

--- 03.References types/apps.js
+++ 03.References types/apps.js
@@ -16,3 +16,22 @@
 // const nestArr = ["one", ["aku", "kamu"], 1, 2, 3];
 
 // console.log(nestArr[1][0]);
+
+// belajar method string
+
+const fruits = ["mango", "banana", "apple"];
+// console.log(fruits);
+
+// fruits.shift();
+
+// console.log(fruits);
+
+// fruits.unshift("rambutan");
+// console.log(fruits);
+
+const moreFruits = ["rambutan", "lengkeng", "durian"];
+// const result = fruits.concat(moreFruits);
+// console.log(result);
+const number = [1, 2, 5, 3, 4];
+
+console.log(number.sort());
